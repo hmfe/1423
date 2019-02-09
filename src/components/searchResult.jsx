4504f09040d965d0b4b261ea1c2952ea473990f2
John Romani <john@anthood.com>
2019-02-09 04:14:06 +0100

    mucho stable, poco limpio

---
+++ src/components/searchResult.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+const SearchResult = (props) => {
+    
+    const Multi = () => {
+        return (
+          <ul>
+          { props.result.map(movie => <li key={movie.id}>{movie.title} ({movie.extra})</li>)}
+          </ul>
+        )
+      }
+    const None = () => <div>Sorry no matches..</div>;
+    const MoreChars = () => <div>More chars plz...</div>;
+      
+    if (props.query.length <= props.minChars) return <MoreChars />
+    else{
+        return  !props.result.length && props.query.length>props.minChars+1? <None />:<Multi />
+    }
+};
+
+export default SearchResult;
+
--- src/components/searcher.jsx
+++ src/components/searcher.jsx
@@ -1,71 +1,108 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component, Fragment } from "react";
+import axios from "axios";
+import SearchResult from "./searchResult";
 
 class Searcher extends Component {
-    state = {
-        error: null,
-        isLoaded: false,
-        result: []
+  state = {
+    error: null,
+    isLoaded: false,
+    result: [],
+    lastquery: "",
+    minChars: 2,
+    active: false
+  };
+
+  async componentDidMount() {
+    const apiKey = "c857fccf3354f1ca866dc08a04b4be34";
+    this.url = "https://api.themoviedb.org/3/search/movie?api_key=" + apiKey;
+    await this.getSearch("testquery");
+  }
+
+  extractDetails(arr) {
+    return arr.map(pos => {
+      const { id, title, release_date } = pos;
+      return { id: id, title: title, extra: release_date };
+    });
+  }
+  async getSearch(query) {
+    try {
+      let data = await this.getJson(this.url + "&query=" + query);
+
+      const r =
+        query === "testquery"
+          ? { isLoaded: true }
+          : { isLoaded: true, result: this.extractDetails(data.results) };
+      this.setState(r);
+    } catch (error) {
+      this.setState({
+        isLoaded: true,
+        error
+      });
+      console.log(
+        "There has been a problem with your fetch operation: ",
+        error.message
+      );
+    }
+  }
+
+  async getJson(url) {
+    let res = await axios.get(url);
+
+    if (res.status === 200) {
+      let json = await res.data;
+      return json;
     }
 
-    async componentDidMount() {
+    throw new Error(res.status);
+  }
 
-        const apiKey = 'c857fccf3354f1ca866dc08a04b4be34';
-	    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + apiKey + '&query=Jack+Reacher';
+  handleChange = e => {
+    this.setState({ active: true });
+    this.setState({ lastquery: e.target.value });
 
-
-        
-        try {
-            let data = await this.getJson(url);
-            this.setState({ isLoaded: true, result:data.results });            
-        }
-        catch(error){
-            this.setState({
-                isLoaded: true,
-                error
-            })
-			console.log('There has been a problem with your fetch operation: ', error.message);
-		};
-        
-      }
-
-      shouldComponentUpdate(nextProps, nextState) {
-        console.log('should ud', nextState);
-        return true
-      }
-
-      componentDidCatch()
-      {
-          console.log( 'componentDidCatch');
-      }
-
-    async getJson(url) {
-        let res = await axios.get(url);
-
-        if (res.status === 200) {
-            let json = await res.data;
-            return json;
-        }
-
-        throw new Error(res.status);
+    if (e.target.value.length > this.state.minChars) {
+      this.resetTimer(200, e.target.value);
+    } else {
+      clearTimeout(this.searchTimeout);
+      this.setState({ result: [] });
     }
+  };
 
-    render() { 
-        console.log( 'render');
-        const { error, isLoaded, result } = this.state;
-        
-        if (error) {
-            return <div>Error: {error.message}</div>;
-          } else if (!isLoaded) {
-            return <div>Loading...</div>;
-          } else {
+  handleBlur = e => {
+    this.setState({ active: false });
+  };
+
+  resetTimer(ms, inputString) {
+    clearTimeout(this.searchTimeout);
+
+    this.searchTimeout = setTimeout(async () => {
+      await this.getSearch(inputString);
+      this.setState({ lastquery: inputString });
+    }, ms);
+  }
+
+  render() {
+    // console.log("** render() **");
+    const { error, result, lastquery, minChars } = this.state;
+
+    if (error) {
+      return <div>Sorry, search is not possible: {error.message}</div>;
+    } else {
+      if (!this.state.active) {
         return (
-            <ul>
-              { this.state.result.map(movie => <li key={movie.id}>{movie.title}</li>)}
-            </ul>
-          )
-        }
+          <Fragment>
+            <input onFocus={this.handleChange} onBlur={this.handleBlur}  />
+          </Fragment>
+        );
+      }
+      return (
+        <Fragment>
+          <input onChange={this.handleChange} onBlur={this.handleBlur} />
+          <SearchResult query={lastquery} minChars={minChars} result={result} />
+        </Fragment>
+      );
     }
+  }
 }
- 
+
 export default Searcher;
