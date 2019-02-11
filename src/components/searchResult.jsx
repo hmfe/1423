f2462922b2da511ccd64f61ed1e359448ecc4189
John Romani <john@anthood.com>
2019-02-11 02:55:45 +0100

    cleaner

--- src/components/searchResult.jsx
+++ src/components/searchResult.jsx
@@ -1,35 +1,26 @@
 import React, { Component } from "react";
 
+const None = () => <div>Sorry no matches..</div>;
+const MoreChars = () => <div>Morex chars plz...</div>;
+const Multi = props => {
+  return props.result.map((movie, i) => (
+    <option className="list-item" key={movie.id} value={movie.id}>
+      {movie.title} ({movie.extra})
+    </option>
+  ));
+};
+
 class SearchResult extends Component {
   render() {
-    const hello = () => {
-      console.log("hello");
-    };
+    let { query, minChars, result } = this.props;
 
-    const Multi = () => {
-      return this.props.result.map((movie, i) => (
-        <option
-          className="list-item"
-          key={movie.id}
-          onClick={hello}
-          value={movie.id}
-        >
-          {movie.title} ({movie.extra})
-        </option>
-      ));
-    };
-
-    const None = () => <div>Sorry no matches..</div>;
-    const MoreChars = () => <div>More chars plz...</div>;
-
-    if (this.props.query.length < this.props.minChars) return <MoreChars />;
+    if (query.length < minChars) return <MoreChars />;
     else {
-      return !this.props.result.length &&
-        this.props.query.length > this.props.minChars + 1 ? (
+      return !result.length && query.length > minChars + 1 ? (
         <None />
       ) : (
-        <Dropdown result={this.props.result}>
-          <Multi />
+        <Dropdown result={result}>
+          <Multi result={result} />
         </Dropdown>
       );
     }
@@ -44,20 +35,11 @@ class Dropdown extends Component {
     lastSelect: ""
   };
 
-  constructor(props) {
-    super(props);
-    this.refSelect = React.createRef();
-  }
-
   handleChange = e => {
     console.log("handleChange");
 
-    let upd = this.refSelect.current.value;
+    let upd = e.target.value;
     this.setState({ lastSelect: upd });
-  };
-
-  handleClick = e => {
-    console.log("handleClick", e.target, this.refSelect.current);
   };
 
   render() {
@@ -72,11 +54,9 @@ class Dropdown extends Component {
         onBlur={() => this.setState({ hasFocus: false })}
         value={this.state.lastSelect}
         onChange={this.handleChange}
-        onClick={this.handleClick}
       >
         {this.props.children}
       </select>
     );
   }
-
 }
